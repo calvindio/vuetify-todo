da7085c100c627f5b7a92201a1302a4dbd4d8318
calvindio <utomo.calvin@gmail.com>
2021-05-26 08:44:25 +0700

    4. padding

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,7 @@
 <template lang="pug">
-.about
-	h1 This is an about page
+.about.pa-6
+	h1 About Vuetify Todo
+	p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+	p Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 </template>
