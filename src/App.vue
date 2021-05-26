4158703243eb5af601080c4c71265dadec4f2b8b
calvindio <utomo.calvin@gmail.com>
2021-05-26 07:20:18 +0700

    ... to pug

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
-<template>
-	<v-app id="inspire">
-		<v-navigation-drawer v-model="drawer" app>
-			<!--  -->
-		</v-navigation-drawer>
-
-		<v-app-bar app>
-			<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-			<v-toolbar-title>Application</v-toolbar-title>
-		</v-app-bar>
-
-		<v-main>
-			<!--  -->
-		</v-main>
-	</v-app>
+<template lang="pug">
+v-app#inspire 
+	v-navigation-drawer(v-model='drawer', app) 
+	v-app-bar(app) 
+		v-app-bar-nav-icon(@click='drawer = !drawer') 
+		v-toolbar-title Application
+	v-main 
 </template>
 
 <script>
