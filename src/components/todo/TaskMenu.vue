512fdd7401fa39ae3b05e408738af90d992ec0fb
calvindio <utomo.calvin@gmail.com>
2021-05-27 06:36:02 +0700

    2. add menu items & icons

--- src/components/todo/TaskMenu.vue
+++ src/components/todo/TaskMenu.vue
@@ -1,9 +1,23 @@
 <template lang="pug">
 v-menu(bottom, left) 
-	<template lang="pug">(v-slot:activator="{ on, attrs }") 
-		v-btn(dark, icon, v-bind='attrs', v-on='on') 
+	template(v-slot:activator='{ on, attrs }') 
+		v-btn(color='primary', icon, v-bind='attrs', v-on='on') 
 			v-icon mdi-dots-vertical
 	v-list 
 		v-list-item(v-for='(item, i) in items', :key='i') 
+			v-list-item-icon
+				v-icon(v-text='item.icon')
 			v-list-item-title {{ item.title }}
 </template>
+
+<script>
+export default {
+	data: () => ({
+		items: [
+			{ title: 'Edit', icon: 'mdi-pencil' },
+			{ title: 'Due Date', icon: 'mdi-calendar' },
+			{ title: 'Delete', icon: 'mdi-delete' },
+		],
+	}),
+}
+</script>
