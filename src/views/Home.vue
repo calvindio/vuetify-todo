038aae699266c7b7e91d689a707f261e21965a42
calvindio <utomo.calvin@gmail.com>
2021-05-26 07:05:38 +0700

    3. clean up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@ hello-world
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 export default {
 	name: 'Home',
 	components: {
