0d3208238c4fc5067e2f0a12cf2303a484b5e5fb
calvindio <utomo.calvin@gmail.com>
2021-05-26 07:27:00 +0700

    2. setup the drawer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template lang="pug">
-v-app#inspire 
-	v-navigation-drawer(v-model='drawer', app) 
-	v-app-bar(app) 
-		v-app-bar-nav-icon(@click='drawer = !drawer') 
-		v-toolbar-title Application
-	v-main 
+v-card.mx-auto(height='400', width='256') 
+	v-navigation-drawer(permanent) 
+		v-list-item
+			v-list-item-content
+				v-list-item-title.title Application
+				v-list-item-subtitle subtext
+		v-divider
+		v-list(dense, nav) 
+			v-list-item(v-for='item in items', :key='item.title', link) 
+				v-list-item-icon
+					v-icon {{ item.icon }}
+				v-list-item-content
+					v-list-item-title {{ item.title }}
 </template>
 
 <script>
 export default {
-	data: () => ({ drawer: null }),
+	data() {
+		return {
+			items: [
+				{ title: 'Dashboard', icon: 'mdi-view-dashboard' },
+				{ title: 'Photos', icon: 'mdi-image' },
+				{ title: 'About', icon: 'mdi-help-box' },
+			],
+			right: null,
+		}
+	},
 }
 </script>
