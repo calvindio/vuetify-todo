6b3049fa74003a54a01998c330f8d8660c8fa6f8
calvindio <utomo.calvin@gmail.com>
2021-05-27 06:02:25 +0700

    6. dismiss snackbar

--- src/components/shared/Snackbar.vue
+++ src/components/shared/Snackbar.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 v-snackbar(v-model='$store.state.snackbar.show') {{ $store.state.snackbar.text }}
 	template(v-slot:action='{ attrs }')
-		v-btn(color='pink', text, v-bind='attrs', @click='snackbar = false') Close
+		v-btn(@click='$store.commit("hideSnackbar")', text, v-bind='attrs') Close
 </template>
 
 <script>
