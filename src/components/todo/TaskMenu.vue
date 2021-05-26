ae81aeb4ee594b1e61061c9cd0199600706d0e88
calvindio <utomo.calvin@gmail.com>
2021-05-27 06:42:24 +0700

    3. handle clicks

--- src/components/todo/TaskMenu.vue
+++ src/components/todo/TaskMenu.vue
@@ -4,7 +4,7 @@ v-menu(bottom, left)
 		v-btn(color='primary', icon, v-bind='attrs', v-on='on') 
 			v-icon mdi-dots-vertical
 	v-list 
-		v-list-item(v-for='(item, i) in items', :key='i') 
+		v-list-item(v-for='(item, i) in items', :key='i', @click='handleClick(i)') 
 			v-list-item-icon
 				v-icon(v-text='item.icon')
 			v-list-item-title {{ item.title }}
@@ -14,10 +14,33 @@ v-menu(bottom, left)
 export default {
 	data: () => ({
 		items: [
-			{ title: 'Edit', icon: 'mdi-pencil' },
-			{ title: 'Due Date', icon: 'mdi-calendar' },
-			{ title: 'Delete', icon: 'mdi-delete' },
+			{
+				title: 'Edit',
+				icon: 'mdi-pencil',
+				click() {
+					console.log('edit')
+				},
+			},
+			{
+				title: 'Due Date',
+				icon: 'mdi-calendar',
+				click() {
+					console.log('due date')
+				},
+			},
+			{
+				title: 'Delete',
+				icon: 'mdi-delete',
+				click() {
+					console.log('delete')
+				},
+			},
 		],
 	}),
+	methods: {
+		handleClick(index) {
+			this.items[index].click()
+		},
+	},
 }
 </script>
