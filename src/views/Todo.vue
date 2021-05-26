f65801367986a0938874da20b1c66a52297da075
calvindio <utomo.calvin@gmail.com>
2021-05-27 04:16:57 +0700

    4. delete task

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -11,6 +11,9 @@
 						v-checkbox(:input-value='task.done') 
 					v-list-item-content 
 						v-list-item-title(:class='{ "text-decoration-line-through": task.done }') {{ task.title }}
+					v-list-item-action 
+						v-btn(@click.stop='deleteTask(task.id)', icon) 
+							v-icon(color='primary lighten-1') mdi-delete
 			v-divider
 </template>
 
@@ -30,6 +33,9 @@ export default {
 		doneTask(id) {
 			let task = this.tasks.filter((task) => task.id === id)[0]
 			task.done = !task.done
+		},
+		deleteTask(id) {
+			this.tasks = this.tasks.filter((task) => task.id !== id)
 		},
 	},
 }
