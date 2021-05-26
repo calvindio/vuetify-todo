8219a23a80c377e7201e7d7d9cba55b4c9bf1d23
calvindio <utomo.calvin@gmail.com>
2021-05-27 05:39:45 +0700

    4. make snackbar text dynamic

--- src/components/shared/Snackbar.vue
+++ src/components/shared/Snackbar.vue
@@ -1,13 +1,9 @@
 <template lang="pug">
-v-snackbar(v-model='$store.state.snackbar.show') {{ text }}
+v-snackbar(v-model='$store.state.snackbar.show') {{ $store.state.snackbar.text }}
 	template(v-slot:action='{ attrs }')
 		v-btn(color='pink', text, v-bind='attrs', @click='snackbar = false') Close
 </template>
 
 <script>
-export default {
-	data: () => ({
-		text: `Task Added`,
-	}),
-}
+export default {}
 </script>
