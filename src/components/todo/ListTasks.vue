5d6f84b561403c435c59fcc24cea915ece9160db
calvindio <utomo.calvin@gmail.com>
2021-05-27 05:13:17 +0700

    4. move task-item to a child component

--- src/components/todo/ListTasks.vue
+++ src/components/todo/ListTasks.vue
@@ -1,17 +1,12 @@
 <template lang="pug">
 v-list.pt-0(flat)
-	div(v-for='task in $store.state.tasks', :key='task.id')
-		v-list-item(
-			@click='$store.commit("doneTask", task.id)',
-			:class='{ "blue darken-4": task.done }'
-		)
-			template(v-slot:default) 
-				v-list-item-action 
-					v-checkbox(:input-value='task.done') 
-				v-list-item-content 
-					v-list-item-title(:class='{ "text-decoration-line-through": task.done }') {{ task.title }}
-				v-list-item-action 
-					v-btn(@click.stop='$store.commit("deleteTask", task.id)', icon) 
-						v-icon(color='primary lighten-1') mdi-delete
-		v-divider
+	task(v-for='task in $store.state.tasks', :key='task.id', :task='task')
 </template>
+
+<script>
+export default {
+	components: {
+		Task: require('@/components/todo/Task').default,
+	},
+}
+</script>
---
+++ src/components/todo/Task.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+div
+	v-list-item(
+		@click='$store.commit("doneTask", task.id)',
+		:class='{ "blue darken-4": task.done }'
+	)
+		template(v-slot:default) 
+			v-list-item-action 
+				v-checkbox(:input-value='task.done') 
+			v-list-item-content 
+				v-list-item-title(:class='{ "text-decoration-line-through": task.done }') {{ task.title }}
+			v-list-item-action 
+				v-btn(@click.stop='$store.commit("deleteTask", task.id)', icon) 
+					v-icon(color='primary lighten-1') mdi-delete
+	v-divider
+</template>
+
+<script>
+export default {
+	props: ['task'],
+}
+</script>
