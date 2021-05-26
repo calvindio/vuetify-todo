72eb231e02652a070fd177a2f626ace936eda5e3
calvindio <utomo.calvin@gmail.com>
2021-05-27 03:58:19 +0700

    2. Task's data

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,16 +1,27 @@
 <template lang="pug">
 .home
-	v-list(flat) 
-		v-list-item 
-			template(v-slot:default='{ active }') 
-				v-list-item-action 
-					v-checkbox(:input-value='active') 
-				v-list-item-content 
-					v-list-item-title Wake up
+	v-list.pt-0(flat)
+		div(v-for='task in tasks', :key='task.id')
+			v-list-item
+				template(v-slot:default='{ active }') 
+					v-list-item-action 
+						v-checkbox(:input-value='active') 
+					v-list-item-content 
+						v-list-item-title {{ task.title }}
+			v-divider
 </template>
 
 <script>
 export default {
 	name: 'Home',
+	data() {
+		return {
+			tasks: [
+				{ id: 1, title: 'Wake up' },
+				{ id: 2, title: 'Get bananas' },
+				{ id: 3, title: 'Eat bananas' },
+			],
+		}
+	},
 }
 </script>
