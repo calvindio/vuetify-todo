73e457cd7bac2585ceeb625e9daf6e037f26a6e3
calvindio <utomo.calvin@gmail.com>
2021-05-27 05:38:10 +0700

    3. show snackbar when task is deleted

--- src/components/todo/Task.vue
+++ src/components/todo/Task.vue
@@ -10,7 +10,7 @@ div
 			v-list-item-content 
 				v-list-item-title(:class='{ "text-decoration-line-through": task.done }') {{ task.title }}
 			v-list-item-action 
-				v-btn(@click.stop='$store.commit("deleteTask", task.id)', icon) 
+				v-btn(@click.stop='$store.dispatch("deleteTask", task.id)', icon) 
 					v-icon(color='primary lighten-1') mdi-delete
 	v-divider
 </template>
