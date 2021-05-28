6aa099d141038aeed1b2734e9c9e6485b1be64be
calvindio <utomo.calvin@gmail.com>
2021-05-29 00:06:27 +0700

    4. use .env variable in HTML page and router

--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,7 @@ const router = new VueRouter({
 	routes,
 })
 router.beforeEach((to, from, next) => {
-	document.title = `Vuetify Todo - ${to.name}`
+	document.title = `${process.env.VUE_APP_TITLE} - ${to.name}`
 	next()
 })
 export default router
