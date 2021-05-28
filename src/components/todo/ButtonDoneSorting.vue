a8bcccd5184f16adc5fda0108a56f00eb4a5a1d4
calvindio <utomo.calvin@gmail.com>
2021-05-29 00:23:36 +0700

    2. add sort menu itme & done sorting button

---
+++ src/components/todo/ButtonDoneSorting.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+v-btn.button-done-sorting(
+	@click='$store.commit("toggleSorting")',
+	color='primary'
+) Done Sorting
+</template>
+
+<style lang="sass">
+.button-done-sorting
+	position: fixed
+	bottom: 20px
+	left: 50%
+	transform: translateX(-50%)
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,6 +3,7 @@
 	field-add-task
 	list-tasks(v-if='$store.state.tasks.length')
 	no-tasks(v-else)
+	button-done-sorting(v-if='$store.state.sorting')
 </template>
 
 <script>
@@ -12,6 +13,7 @@ export default {
 		FieldAddTask: require('@/components/todo/FieldAddTask').default,
 		ListTasks: require('@/components/todo/ListTasks').default,
 		NoTasks: require('@/components/todo/NoTasks').default,
+		ButtonDoneSorting: require('@/components/todo/ButtonDoneSorting').default,
 	},
 }
 </script>
