9d58265de3e76ef2830a4c1fef4c7c1e05a2f55c
calvindio <utomo.calvin@gmail.com>
2021-05-28 22:42:23 +0700

    2. show current date

--- src/components/tools/LiveDateTime.vue
+++ src/components/tools/LiveDateTime.vue
@@ -1,3 +1,20 @@
 <template lang="pug">
-.text-subtitle-1.ml-4 October 20, 14:40
+.text-subtitle-1.ml-4 {{ date }}
 </template>
+
+<script>
+import { format } from 'date-fns'
+export default {
+	data() {
+		return { date: '' }
+	},
+	methods: {
+		getDate() {
+			this.date = format(new Date(), 'MMM d')
+		},
+	},
+	mounted() {
+		this.getDate()
+	},
+}
+</script>
